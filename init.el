554e21ec668d4f356c54b103991b55e6c65ada01
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2015-05-15 18:53:32 +0530

    Latex-preview-pane to auctex

--- init.el
+++ init.el
@@ -65,6 +65,20 @@
 
 ;;(setq tabbar-use-images nil)
 
+;; Proper Unicode
+(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+;; backwards compatibility as default-buffer-file-coding-system
+;; is deprecated in 23.2.
+(if (boundp 'buffer-file-coding-system)
+    (setq-default buffer-file-coding-system 'utf-8)
+  (setq default-buffer-file-coding-system 'utf-8))
+
+;; Treat clipboard input as UTF-8 string first; compound text next, etc.
+(setq x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING))
+
 ;; Sr Speedbar stuff
 (require 'sr-speedbar)
   (setq 
