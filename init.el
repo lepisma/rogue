a3cf1d120998b30c41d2c02e2b84a15df23ef7e1
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2015-05-17 18:09:23 +0530

    Change theme

--- init.el
+++ init.el
@@ -3,6 +3,13 @@
 ;;; Code:
 
 ;; *important* Change the path to cask install directory
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (require 'cask "~/.cask/cask.el")
 
 ;; Setup cask and pallet
@@ -15,7 +22,7 @@
 ;; Graphene does the basic setup
 (require 'graphene)
 ;; Tweaks
-(load-theme 'tsdh-dark)
+(load-theme 'gotham)
 ;; Disable menu bar
 (menu-bar-mode -1)
 ;; CUA Mode
@@ -36,34 +43,6 @@
 
 ;; Desktop save
 (desktop-save-mode 1)
-
-;; Tabbar
-;; (setq tabbar-ruler-global-tabbar t)
-;; (setq tabbar-ruler-global-ruler t)
-;; (require 'tabbar-ruler) 
-
-;; (require 'tabbar)
-;; (tabbar-mode t)
-;; (defun tabbar-buffer-groups ()
-;;   (list
-;;    (cond
-;;     ((string-equal "*" (substring (buffer-name) 0 1))
-;;      "Emacs Buffer"
-;;      )
-;;     ((eq major-mode 'dired-mode)
-;;      "Dired"
-;;      )
-;;     (t
-;;      "User Buffer"
-;;      )
-;;     )))
-
-;; (setq tabbar-buffer-groups-function 'tabbar-buffer-groups)
-
-;; (global-set-key [M-left] 'tabbar-backward)
-;; (global-set-key [M-right] 'tabbar-forward)
-
-;;(setq tabbar-use-images nil)
 
 ;; Proper Unicode
 (prefer-coding-system 'utf-8)
@@ -116,7 +95,7 @@
 ;; org mode special-symbol-mode
 (setq-default org-pretty-entities 1)
 
-;; trigger flypell in latex mode
+;; trigger flypell in latex and org mode
 (add-hook 'latex-mode-hook (lambda ()
                              (flyspell-mode 1)
                              ))
@@ -132,7 +111,10 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("bfa3d52c7e3bbf528760bdbb8b59a69beda8d8b257d60a1b3ac26c1e5bc190bb" default))))
+    ("2d7e4feac4eeef3f0610bf6b155f613f372b056a2caae30a361947eab5074716" "c158c2a9f1c5fcf27598d313eec9f9dceadf131ccd10abc6448004b14984767c" "7f5837a7dbf54c2b7c41d94f5eb1373cf63274847d1971037faa24d7f2231eea" "86201c0dccf07a21ce323e124ee9c89d04bbe4f5067446e6492b6ea82265b2d6" "85ef1f4095ad38ed2744577c00e5f1f8dc0000d5015024d50943a1808495f56c" "4695c919c56c1d81fb62d1a7c1cc40d0b365d766f053be2a25df08565bdbd793" "bfa3d52c7e3bbf528760bdbb8b59a69beda8d8b257d60a1b3ac26c1e5bc190bb" default)))
+ '(package-selected-packages
+   (quote
+    (green-phosphor-theme phoenix-dark-mono-theme ample-theme tabbar-ruler scss-mode rainbow-mode rainbow-delimiters projectile processing-mode powerline pallet multiple-cursors matlab-mode markdown-mode magit lua-mode julia-mode helm graphene elpy cmake-mode auctex ac-cider))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
