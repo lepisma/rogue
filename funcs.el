7d5635a47f6b83761e08b0d9c4cde5961fbc055f
lepisma <abhinav.tushar.vs@gmail.com>
2015-10-03 20:06:31 +0530

    Add kill-other-buffers function

---
+++ funcs.el
@@ -0,0 +1,9 @@
+;;; funcs.el --- rogue Layer utility functions
+
+
+(defun kill-other-buffers ()
+  "Kill all other buffers."
+  (interactive)
+  (mapc 'kill-buffer
+        (delq (current-buffer)
+              (remove-if-not 'buffer-file-name (buffer-list)))))
