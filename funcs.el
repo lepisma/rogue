be4f9b2d5119e32984e0fd2aa1185fbe1df1b240
Abhinav Tushar <Abhinav Tushar>
2016-01-31 02:57:19 +0530

    Open explorer here functions

--- funcs.el
+++ funcs.el
@@ -7,3 +7,10 @@
   (mapc 'kill-buffer
         (delq (current-buffer)
               (remove-if-not 'buffer-file-name (buffer-list)))))
+
+(defun explore-here ()
+  "Open file manager in current buffer's directory."
+  (interactive)
+  (if (eq system-type 'windows-nt)
+      (shell-command "explorer .")
+    (message "Only on Windows")))
