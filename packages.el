a0478d36edacd781ec7efc1f70133d87acca189b
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2017-02-22 05:43:52 -0500

    Fix bm settings

--- packages.el
+++ packages.el
@@ -48,24 +48,25 @@
 
 (defun rogue/init-bm ()
   (use-package bm
-    :defer t
+    :ensure t
+    :demand t
     :init
     ;; restore on load (even before you require bm)
     (setq bm-restore-repository-on-load t)
     :config
+    (set-face-attribute 'bm-persistent-face nil :background "DarkCyan" :foreground "white")
     ;; Allow cross-buffer 'next'
     (setq bm-cycle-all-buffers t)
     ;; where to store persistant files
-    (setq bm-repository-file "~/.emacs.d/bm-repository")
-    (set-face-attribute 'bm-persistent-face nil :background "DarkCyan" :foreground "white")
+    (setq bm-repository-file "~/.emacs.d/.cache/bm-repository")
     ;; save bookmarks
     (setq-default bm-buffer-persistence t)
     ;; Loading the repository from file when on start up.
-    (add-hook' after-init-hook 'bm-repository-load)
+    (add-hook 'after-init-hook 'bm-repository-load)
     ;; Restoring bookmarks when on file find.
     (add-hook 'find-file-hooks 'bm-buffer-restore)
     ;; Saving bookmarks
-    (add-hook 'kill-buffer-hook #'bm-buffer-save)
+    (add-hook 'kill-buffer-hook 'bm-buffer-save)
     ;; Saving the repository to file when on exit.
     ;; kill-buffer-hook is not called when Emacs is killed, so we
     ;; must save all bookmarks first.
@@ -76,10 +77,10 @@
     ;; The `after-save-hook' is not necessary to use to achieve persistence,
     ;; but it makes the bookmark data in repository more in sync with the file
     ;; state.
-    (add-hook 'after-save-hook #'bm-buffer-save)
+    (add-hook 'after-save-hook 'bm-buffer-save)
     ;; Restoring bookmarks
-    (add-hook 'find-file-hooks   #'bm-buffer-restore)
-    (add-hook 'after-revert-hook #'bm-buffer-restore)
+    (add-hook 'find-file-hooks   'bm-buffer-restore)
+    (add-hook 'after-revert-hook 'bm-buffer-restore)
     ;; The `after-revert-hook' is not necessary to use to achieve persistence,
     ;; but it makes the bookmark data in repository more in sync with the file
     ;; state. This hook might cause trouble when using packages
@@ -88,7 +89,7 @@
     ;; called before the buffer is reverted (like `vc-before-checkin-hook').
     ;; Then new bookmarks can be saved before the buffer is reverted.
     ;; Make sure bookmarks is saved before check-in (and revert-buffer)
-    (add-hook 'vc-before-checkin-hook #'bm-buffer-save)
+    (add-hook 'vc-before-checkin-hook 'bm-buffer-save)
     :bind (("C-c b" . bm-toggle)
            ("C-c /" . bm-next)
            ("C-c '" . bm-previous))))
