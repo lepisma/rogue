27bef75bf23891145aed423c199f1782d1f5f247
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2017-04-22 16:22:43 -0400

    Secrets stub

--- setup.sh
+++ setup.sh
@@ -4,3 +4,8 @@ git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 git clone git@github.com:lepisma/rogue ~/.emacs.d/private/rogue
 ln -s ~/.emacs.d/private/rogue/.spacemacs ~/.spacemacs
 touch ~/.emacs-custom.el
+
+# Secret files stubs
+mkdir -p ~/.emacs.d/private/rogue/secrets
+touch ~/.emacs.d/private/rogue/secrets/slack.el
+touch ~/.emacs.d/private/rogue/secrets/wolfram.el
