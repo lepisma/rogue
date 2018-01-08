b302585b21a5d6fb2f2d1ad17d20f2c6730296e0
Abhinav Tushar <lepisma@fastmail.com>
2018-01-08 10:20:36 -0500

    bbq integration

---
+++ local/org-bbq/org-bbq.el
@@ -0,0 +1,41 @@
+;;; org-bbq.el --- bbq playlists in org
+
+;; Copyright (c) 2018 Abhinav Tushar
+
+;; Author: Abhinav Tushar <lepisma@fastmail.com>
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "25"))
+;; URL: https://github.com/lepisma/org-bbq.el
+
+;;; Commentary:
+
+;; This file is not a part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+;;; Code:
+
+(require 'org)
+(require 's)
+
+(org-add-link-type "bbq" 'org-bbq-play)
+
+(defun org-bbq-play (path)
+  (apply #'start-process "bbq" nil "bbq" (s-split " " path)))
+
+(provide 'org-bbq)
+
+;;; org-bbq.el ends here
