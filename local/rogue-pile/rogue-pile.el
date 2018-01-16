c31b63f3fe15139586e2054ac0a57cd5c8308580
Abhinav Tushar <lepisma@fastmail.com>
2018-01-15 22:28:18 -0500

    Basic pile setup

---
+++ local/rogue-pile/rogue-pile.el
@@ -0,0 +1,69 @@
+;;; rogue-pile.el --- Pile management
+
+;; Copyright (c) 2018 Abhinav Tushar
+
+;; Author: Abhinav Tushar <lepisma@fastmail.com>
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "25"))
+;; URL: https://github.com/lepisma/rogue-pile.el
+
+;;; Commentary:
+
+;; Org pile management
+;; This file is not a part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+;;; Code:
+
+(require 'org)
+(require 'ox-html)
+(require 'ox-publish)
+
+(defun rogue-pile-setup ()
+  "Setup for pile"
+  (let ((pile-source (concat user-project-dir "pile/pile/"))
+        (pile-output (concat user-project-dir "pile/docs/")))
+    (setq org-publish-project-alist
+          `(("pile-pages"
+             :auto-sitemap t
+             :sitemap-filename "sitemap.org"
+             :sitemap-title "Sitemap"
+             :base-directory ,pile-source
+             :base-extension "org"
+             :recursive t
+             :publishing-directory ,pile-output
+             :publishing-function (org-html-publish-to-html
+                                   org-org-publish-to-org)
+             :htmlized-source t
+             :html-checkbox-type unicode
+             :html-doctype "html5"
+             :html-html5-fancy t
+             :html-postamble t
+             :html-preamble t
+             :makeindex t)
+            ("pile-static"
+             :base-directory ,pile-source
+             :base-extension ".*"
+             :exclude ".*\.org"
+             :recursive t
+             :publishing-directory ,pile-output
+             :publishing-function org-publish-attachment)
+            ("pile" :components ("pile-pages" "pile-static"))))))
+
+(provide 'rogue-pile)
+
+;;; rogue-pile.el ends here
