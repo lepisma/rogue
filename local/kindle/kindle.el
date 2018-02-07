4f9bf2eefd82c2e816c6aee72905427a40175fd9
Abhinav Tushar <lepisma@fastmail.com>
2018-02-07 01:49:52 -0500

    Formatting options

--- local/kindle/kindle.el
+++ local/kindle/kindle.el
@@ -36,20 +36,31 @@
 
 (defconst kindle-mount-path "/proc/mounts")
 
-(defun kindle-read-clippings-file ()
-  "Read clippings file from mounted kindle"
+(defcustom kindle-clipping-format
+  "#+BEGIN_QUOTE
+%s
+
+#+HTML:<footer>%a</footer>
+#+END_QUOTE"
+  "Format for pretty printing a clipping.
+- %s is replaced by the main text
+- %t is for date (and time) added
+- %a is for book and author")
+
+(defun kindle-check-device ()
+  "Check if kindle is mounted"
   (let ((mount-line (->> (f-read-text kindle-mount-path)
                        (s-split "\n")
                        (-find (-cut s-contains? "Kindle" <>)))))
-    (if mount-line
-        (f-read-text (f-join (second (s-split " " mount-line))
-                             "documents"
-                             "My Clippings.txt"))
-      (message "Kindle not mounted"))))
+    mount-line))
 
-(defun kindle-parse-clipping (clipping)
+(defun kindle-read-clippings-file (mount-directory)
+  "Read clippings file from the kindle mount directory"
+  (f-read-text (f-join mount-directory "documents" "My Clippings.txt")))
+
+(defun kindle-parse-clipping (clipping-text)
   "Parse a clipping text"
-  (let* ((lines (s-split "\n" (s-trim clipping)))
+  (let* ((lines (s-split "\n" (s-trim clipping-text)))
          (source (s-trim (car lines)))
          (added (->> (s-split "| Added on " (second lines)) (second) (s-trim)))
          (text (s-trim (s-join "\n" (nthcdr 2 lines)))))
@@ -60,6 +71,13 @@
   (let ((clippings (s-split "==========" (s-trim text))))
     (-map #'kindle-parse-clipping
           (-remove (-cut string-equal <> "") clippings))))
+
+(defun kindle-pretty-print-clipping (clipping)
+  "Pretty print a clipping using the format specified in kindle-clipping-format"
+  (s-replace-all '(("%s" . (nth 0 clipping))
+                   ("%a" . (nth 1 clipping))
+                   ("%t" . (nth 2 clipping)))
+                 kindle-clipping-format))
 
 (provide 'kindle)
 
