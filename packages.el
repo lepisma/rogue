eecf1b459481654df4eb971b86d527bdc6dadcd8
Abhinav Tushar <lepisma@fastmail.com>
2018-08-08 02:59:06 +0530

    Macro for package definitions

--- packages.el
+++ packages.el
@@ -1,5 +1,10 @@
 ;;; packages.el --- rogue Layer packages File for Spacemacs
 
+(defmacro rpkg (name &rest body)
+  (declare (indent defun))
+  `(defun ,(intern (format "rogue/init-%s" (symbol-name name))) ()
+     (use-package ,name ,@body)))
+
 (defconst rogue-packages
   '(all-the-icons
     (authinfo :location local)
@@ -56,254 +61,211 @@
     writegood-mode))
 
 ;; Initialize packages
-(defun rogue/init-all-the-icons ()
-  (use-package all-the-icons))
+(rpkg all-the-icons)
 
-(defun rogue/init-authinfo ()
-  (use-package authinfo
-    :after (s dash-functional)))
+(rpkg authinfo
+  :after (s dash-functional))
 
-(defun rogue/init-beacon ()
+(rpkg beacon
   :config
-  (beacon-mode +1)
+  (beacon-mode)
   (setq beacon-color (face-attribute 'region :background nil t)
         beacon-blink-when-buffer-changes t
         beacon-blink-when-point-moves-vertically nil))
 
-(defun rogue/init-bmp ()
-  (use-package bmp))
+(rpkg bmp)
 
-(defun rogue/init-browse-at-remote ()
-  (use-package browse-at-remote
-    :defer t))
+(rpkg browse-at-remote)
 
-(defun rogue/init-cricbuzz ()
-  (use-package cricbuzz
-    :defer t))
+(rpkg cricbuzz)
 
-(defun rogue/init-calibre ()
-  (use-package calibre
-    :after (s dash-functional)
-    :config
-    (setq calibre-root (concat user-cloud-dir "Calibre Shared"))))
+(rpkg calibre
+  :after (s dash-functional)
+  :config
+  (setq calibre-root (concat user-cloud-dir "Calibre Shared")))
 
-(defun rogue/init-colormaps ()
-  (use-package colormaps
-    :defer t))
+(rpkg colormaps)
 
-(defun rogue/init-company-box ()
-  (use-package company-box
-    :after company
-    :config
-    (add-hook 'text-mode-hook #'company-box-mode)))
+(rpkg company-box
+  :after company
+  :config
+  (add-hook 'text-mode-hook #'company-box-mode))
 
-(defun rogue/init-conda ()
-  (use-package conda
-    :config
-    (conda-env-initialize-eshell)
-    (setq conda-anaconda-home (expand-file-name "~/.miniconda"))))
+(rpkg conda
+  :config
+  (conda-env-initialize-eshell)
+  (setq conda-anaconda-home (expand-file-name "~/.miniconda")))
 
-(defun rogue/init-dired-subtree ()
-  (use-package dired-subtree :ensure t
-    :after ranger
-    :config
-    (bind-key "<tab>" 'dired-subtree-toggle ranger-mode-map)
-    (bind-key "<backtab>" 'dired-subtree-cycle ranger-mode-map)))
+(rpkg dired-subtree
+  :after ranger
+  :config
+  (bind-key "<tab>" 'dired-subtree-toggle ranger-mode-map)
+  (bind-key "<backtab>" 'dired-subtree-cycle ranger-mode-map))
 
-(defun rogue/init-doom-themes ()
-  (use-package doom-themes
-    :config
-    (setq doom-themes-enable-bold t
-          doom-themes-enable-italic t)
+(rpkg doom-themes
+  :config
+  (setq doom-themes-enable-bold t
+        doom-themes-enable-italic t)
 
-    (doom-themes-neotree-config)
-    (setq doom-neotree-enable-variable-pitch t
-          doom-neotree-file-icons 'simple
-          doom-neotree-line-spacing 4)
-    (doom-themes-org-config)))
+  (doom-themes-neotree-config)
+  (setq doom-neotree-enable-variable-pitch t
+        doom-neotree-file-icons 'simple
+        doom-neotree-line-spacing 4)
+  (doom-themes-org-config))
 
-(defun rogue/init-elnode ()
-  (use-package elnode))
+(rpkg elnode)
 
-(defun rogue/init-enlive ()
-  (use-package enlive))
+(rpkg enlive)
 
-(defun rogue/init-gscholar-bibtex ()
-  (use-package gscholar-bibtex
-    :config
-    (setq gscholar-bibtex-database-file user-bib-file
-          gscholar-bibtex-default-source "Google Scholar")))
+(rpkg gscholar-bibtex
+  :config
+  (setq gscholar-bibtex-database-file user-bib-file
+        gscholar-bibtex-default-source "Google Scholar"))
 
-(defun rogue/init-hy-mode ()
-  (use-package hy-mode
-    :mode "\\.hy\\'"))
+(rpkg hy-mode
+  :mode "\\.hy\\'")
 
-(defun rogue/init-kindle ()
-  (use-package kindle
-    :config
-    (setq kindle-clipping-save-file user-clippings-file)))
+(rpkg kindle
+  :config
+  (setq kindle-clipping-save-file user-clippings-file))
 
-(defun rogue/init-levenshtein ()
-  (use-package levenshtein))
+(rpkg levenshtein)
 
-(defun rogue/init-mpm ()
-  (use-package mpm
-    :demand t
-    :after org-cliplink))
+(rpkg mpm
+  :demand t
+  :after org-cliplink)
 
-(defun rogue/init-multiple-cursors ()
-  (use-package multiple-cursors
-    :bind (("C->" . mc/mark-next-like-this)
-           ("C-<" . mc/mark-previous-like-this)
-           ("C-M-<mouse-1>" . mc/add-cursor-on-click))))
+(rpkg multiple-cursors
+  :bind (("C->" . mc/mark-next-like-this)
+         ("C-<" . mc/mark-previous-like-this)
+         ("C-M-<mouse-1>" . mc/add-cursor-on-click)))
 
-(defun rogue/init-ob-async ()
-  (use-package ob-async))
+(rpkg ob-async)
 
-(defun rogue/init-ob-sagemath ()
-  (use-package ob-sagemath))
+(rpkg ob-sagemath)
 
-(defun rogue/init-openwith ()
-  (use-package openwith
-    :demand t
-    :config
-    (setq openwith-associations
-          (list
-           (list (openwith-make-extension-regexp
-                  '("mpg" "mpeg" "mp3" "mp4"
-                    "avi" "wmv" "wav" "mov" "flv"
-                    "ogm" "ogg" "mkv"))
-                 "vlc"
-                 '(file))
-           (list (openwith-make-extension-regexp
-                  '("doc" "xls" "ppt" "odt" "ods" "odg" "odp"))
-                 "libreoffice"
-                 '(file))
-           (list (openwith-make-extension-regexp
-                  '("pdf" "ps" "ps.gz" "dvi"))
-                 "okular"
-                 '(file))))
-    (openwith-mode t)))
+(rpkg openwith
+  :demand t
+  :config
+  (setq openwith-associations
+        (list
+         (list (openwith-make-extension-regexp
+                '("mpg" "mpeg" "mp3" "mp4"
+                  "avi" "wmv" "wav" "mov" "flv"
+                  "ogm" "ogg" "mkv"))
+               "vlc"
+               '(file))
+         (list (openwith-make-extension-regexp
+                '("doc" "xls" "ppt" "odt" "ods" "odg" "odp"))
+               "libreoffice"
+               '(file))
+         (list (openwith-make-extension-regexp
+                '("pdf" "ps" "ps.gz" "dvi"))
+               "okular"
+               '(file))))
+  (openwith-mode))
 
-(defun rogue/init-org-bbq ()
-  (use-package org-bbq
-    :after org))
+(rpkg org-bbq
+  :after org)
 
-(defun rogue/init-org-books ()
-  (use-package org-books
-    :config
-    (setq org-books-file user-books-file)))
+(rpkg org-books
+  :config
+  (setq org-books-file user-books-file))
 
-(defun rogue/init-org-cliplink ()
-  (use-package org-cliplink
-    :bind (("C-c y" . org-cliplink))))
+(rpkg org-cliplink
+  :bind (("C-c y" . org-cliplink)))
 
-(defun rogue/init-org-expand ()
-  (use-package org-expand
-    :bind (("C-c x" . helm-org-expand))))
+(rpkg org-expand
+  :bind (("C-c x" . helm-org-expand)))
 
-(defun rogue/init-org-gh ()
-  (use-package org-gh
-    :after org))
+(rpkg org-gh
+  :after org)
 
-(defun rogue/init-org-journal ()
-  (use-package org-journal
-    :config
-    (setq org-journal-dir user-journal-dir)
-    (setq org-journal-enable-encryption t)))
+(rpkg org-journal
+  :config
+  (setq org-journal-dir user-journal-dir)
+  (setq org-journal-enable-encryption t))
 
-(defun rogue/init-org-pretty-table ()
-  (use-package org-pretty-table
-    :demand t
-    :init
-    (add-hook 'org-mode-hook (lambda () (org-pretty-table-mode 1)))))
+(rpkg org-pretty-table
+  :demand t
+  :init
+  (add-hook 'org-mode-hook #'org-pretty-table-mode))
 
-(defun rogue/init-org-make ()
-  (use-package org-make
-    :after org))
+(rpkg org-make
+  :after org)
 
-(defun rogue/init-org-super-agenda ()
-  (use-package org-super-agenda
-    :after org
-    :config
-    (org-super-agenda-mode)))
+(rpkg org-super-agenda
+  :after org
+  :config
+  (org-super-agenda-mode))
 
-(defun rogue/init-org-web-tools ()
-  (use-package org-web-tools
-    :after org))
+(rpkg org-web-tools
+  :after org)
 
-(defun rogue/init-parinfer ()
-  (use-package parinfer
-    :ensure t
-    :bind (("C-," . parinfer-toggle-mode))
-    :hook ((clojure-mode . parinfer-mode)
-           (emacs-lisp-mode . parinfer-mode)
-           (common-lisp-mode . parinfer-mode)
-           (racket-mode . parinfer-mode)
-           (lisp-mode . parinfer-mode)
-           (scheme-mode . parinfer-mode)
-           (hy-mode . parinfer-mode))
-    :init
-    (setq parinfer-extensions '(defaults
-                                pretty-parens
-                                smart-tab
-                                smart-yank))))
+(rpkg parinfer
+  :bind (("C-," . parinfer-toggle-mode))
+  :hook ((clojure-mode . parinfer-mode)
+         (emacs-lisp-mode . parinfer-mode)
+         (common-lisp-mode . parinfer-mode)
+         (racket-mode . parinfer-mode)
+         (lisp-mode . parinfer-mode)
+         (scheme-mode . parinfer-mode)
+         (hy-mode . parinfer-mode))
+  :init
+  (setq parinfer-extensions '(defaults
+                               pretty-parens
+                               smart-tab
+                               smart-yank)))
 
-(defun rogue/init-pretty-mode ()
-  (use-package pretty-mode
-    :config
-    (global-pretty-mode t)
-    (global-prettify-symbols-mode 1)
+(rpkg pretty-mode
+  :config
+  (global-pretty-mode t)
+  (global-prettify-symbols-mode 1)
 
-    (pretty-deactivate-groups
-     '(:equality
-       :ordering
-       :ordering-double
-       :ordering-triple
-       :arrows
-       :arrows-twoheaded
-       :punctuation
-       :logic
-       :sets
-       :sub-and-superscripts
-       :subscripts
-       :arithmetic-double
-       :arithmetic-triple))
+  (pretty-deactivate-groups
+   '(:equality
+     :ordering
+     :ordering-double
+     :ordering-triple
+     :arrows
+     :arrows-twoheaded
+     :punctuation
+     :logic
+     :sets
+     :sub-and-superscripts
+     :subscripts
+     :arithmetic-double
+     :arithmetic-triple))
 
-    (pretty-activate-groups
-     '(:greek :arithmetic-nary))))
+  (pretty-activate-groups
+   '(:greek :arithmetic-nary)))
 
-(defun rogue/init-read-lyrics ()
-  (use-package read-lyrics
-    :after (s spotify levenshtein)))
+(rpkg read-lyrics
+  :after (s spotify levenshtein))
 
-(defun rogue/init-rogue-ligatures ()
-  (use-package rogue-ligatures
-    :after rogue-utils
-    :config
-    (rogue-ligatures-setup-general)
-    (rogue-ligatures-setup-ess)))
 
-(defun rogue/init-rogue-mu4e ()
-  (use-package rogue-mu4e
-    :after (authinfo mu4e openwith)
-    :config
-    (rogue-mu4e-setup)))
+(rpkg rogue-ligatures
+  :after rogue-utils
+  :config
+  (rogue-ligatures-setup-general)
+  (rogue-ligatures-setup-ess))
 
-(defun rogue/init-rogue-org ()
-  (use-package rogue-org
-    :after org
-    :config
-    (rogue-org-setup-general)
-    (rogue-org-setup-notes)
-    (rogue-org-setup-babel)
-    (rogue-org-setup-tex)))
+(rpkg rogue-mu4e
+  :after (authinfo mu4e openwith)
+  :config (rogue-mu4e-setup))
 
-(defun rogue/init-pile ()
-  (use-package pile
-    :after w
-    :config
-    (let ((preamble-template "<header>
+(rpkg rogue-org
+  :after org
+  :config
+  (rogue-org-setup-general)
+  (rogue-org-setup-notes)
+  (rogue-org-setup-babel)
+  (rogue-org-setup-tex))
+
+(rpkg pile
+  :after w
+  :config
+  (let ((preamble-template "<header>
                                 <div class='site-title'>
                                   <a href='/'>
                                     <img src='/assets/images/avatar32.png'>
@@ -322,122 +284,103 @@
                                 <div class='page-meta'>%s</div>
                                 <h1>%%t</h1>
                               </div>")
-          (postamble "<footer id='footer'></footer>")
-          (output-dir (concat user-project-dir "lepisma.github.io-deploy/")))
-      (setq pile-serve-dir output-dir
-            pile-projects
-            (list (pile-project :name "wiki"
-                                :base-url "wiki"
-                                :input-dir (concat user-project-dir "lepisma.github.io/wiki")
-                                :output-dir (concat output-dir "wiki")
-                                :type 'wiki
-                                :postamble postamble
-                                :preamble (format preamble-template "" "" "active" "Last modified: %d %C"))
-                  (pile-project :name "blog"
-                                :base-url ""
-                                :input-dir (concat user-project-dir "lepisma.github.io/blog")
-                                :output-dir output-dir
-                                :type 'blog
-                                :postamble postamble
-                                :preamble (format preamble-template "active" "" "" "%d"))
-                  (pile-project :name "journal"
-                                :base-url "journal"
-                                :input-dir (concat user-project-dir "lepisma.github.io/journal")
-                                :output-dir (concat output-dir "journal")
-                                :type 'blog
-                                :postamble postamble
-                                :preamble (format preamble-template "" "active" "" "%d"))
-                  (pile-project :name "assets"
-                                :input-dir (concat user-project-dir "lepisma.github.io/assets")
-                                :output-dir (concat output-dir "assets")
-                                :type 'static)
-                  (pile-project :name "misc"
-                                :base-url ""
-                                :input-dir (concat user-project-dir "lepisma.github.io/misc")
-                                :output-dir output-dir
-                                :type 'blog
-                                :postamble ""
-                                :preamble "")))
-      (pile-setup))))
+        (postamble "<footer id='footer'></footer>")
+        (output-dir (concat user-project-dir "lepisma.github.io-deploy/")))
+    (setq pile-serve-dir output-dir
+          pile-projects
+          (list (pile-project :name "wiki"
+                              :base-url "wiki"
+                              :input-dir (concat user-project-dir "lepisma.github.io/wiki")
+                              :output-dir (concat output-dir "wiki")
+                              :type 'wiki
+                              :postamble postamble
+                              :preamble (format preamble-template "" "" "active" "Last modified: %d %C"))
+                (pile-project :name "blog"
+                              :base-url ""
+                              :input-dir (concat user-project-dir "lepisma.github.io/blog")
+                              :output-dir output-dir
+                              :type 'blog
+                              :postamble postamble
+                              :preamble (format preamble-template "active" "" "" "%d"))
+                (pile-project :name "journal"
+                              :base-url "journal"
+                              :input-dir (concat user-project-dir "lepisma.github.io/journal")
+                              :output-dir (concat output-dir "journal")
+                              :type 'blog
+                              :postamble postamble
+                              :preamble (format preamble-template "" "active" "" "%d"))
+                (pile-project :name "assets"
+                              :input-dir (concat user-project-dir "lepisma.github.io/assets")
+                              :output-dir (concat output-dir "assets")
+                              :type 'static)
+                (pile-project :name "misc"
+                              :base-url ""
+                              :input-dir (concat user-project-dir "lepisma.github.io/misc")
+                              :output-dir output-dir
+                              :type 'blog
+                              :postamble ""
+                              :preamble "")))
+    (pile-setup)))
 
-(defun rogue/init-rogue-processes ()
-  (use-package rogue-processes
-    :after rogue-utils
-    :config
-    (rogue-processes-define "offlineimap" "-o")
-    (rogue-processes-define "mpm-play")
-    ;; (rogue-processes-start-service "mpm-play")
-    (setq rogue-processes-git-update-dirs nil)
-    (rogue-processes-run-git-autoupdate-loop "10 min" 3600)))
+(rpkg rogue-processes
+  :after rogue-utils
+  :config
+  (rogue-processes-define "offlineimap" "-o")
+  (rogue-processes-define "mpm-play")
+  (setq rogue-processes-git-update-dirs nil)
+  (rogue-processes-run-git-autoupdate-loop "10 min" 3600))
 
-(defun rogue/init-rogue-ui ()
-  (use-package rogue-ui
-    :after rogue-utils
-    :config
-    (rogue-ui-setup)))
+(rpkg rogue-ui
+  :after rogue-utils
+  :config
+  (rogue-ui-setup))
 
-(defun rogue/init-rogue-utils ()
-  (use-package rogue-utils))
+(rpkg rogue-utils)
 
-(defun rogue/init-sage-shell-mode ()
-  (use-package sage-shell-mode
-    :config
-    (sage-shell:define-alias)
-    (setq sage-shell-view-default-resolution 150)
-    (add-hook 'sage-shell-mode-hook #'eldoc-mode)
-    (add-hook 'sage-shell:sage-mode-hook #'eldoc-mode)
-    (add-hook 'sage-shell-after-prompt-hook #'sage-shell-view-mode)))
+(rpkg sage-shell-mode
+  :config
+  (sage-shell:define-alias)
+  (setq sage-shell-view-default-resolution 150)
+  (add-hook 'sage-shell-mode-hook #'eldoc-mode)
+  (add-hook 'sage-shell:sage-mode-hook #'eldoc-mode)
+  (add-hook 'sage-shell-after-prompt-hook #'sage-shell-view-mode))
 
-(defun rogue/init-shell-switcher ()
-  (use-package shell-switcher
-    :demand t
-    :config (setq shell-switcher-mode t)
-    :bind (("C-'" . shell-switcher-switch-buffer)
-           ("C-\"" . shell-switcher-new-shell))))
+(rpkg shell-switcher
+  :config (setq shell-switcher-mode t)
+  :bind (("C-'" . shell-switcher-switch-buffer)
+         ("C-\"" . shell-switcher-new-shell)))
 
-(defun rogue/init-snakemake-mode ()
-  (use-package snakemake-mode
-    :defer t))
+(rpkg snakemake-mode)
 
-(defun rogue/init-solaire-mode ()
-  (use-package solaire-mode
-    :hook ((prog-mode . turn-on-solaire-mode)
-           (minibuffer-setup . solaire-mode-in-minibuffer)
-           (ediff-prepare-buffer . solaire-mode))))
+(rpkg solaire-mode
+  :hook ((prog-mode . turn-on-solaire-mode)
+         (minibuffer-setup . solaire-mode-in-minibuffer)
+         (ediff-prepare-buffer . solaire-mode)))
 
-(defun rogue/init-spaceline-all-the-icons ()
-  (progn
-    (use-package spaceline-all-the-icons
-      :after spaceline)
-    (use-package spaceline
-      :after powerline
-      :config
-      (setq-default mode-line-format '("%e" (:eval (spaceline-ml-ati)))))))
+(rpkg spaceline
+  :after powerline)
 
-(defun rogue/init-swiper ()
-  (use-package swiper
-    :bind (("C-s" . swiper)
-           ("C-r" . swiper))))
+(rpkg spaceline-all-the-icons
+  :after spaceline
+  :config
+  (setq-default mode-line-format '("%e" (:eval (spaceline-ml-ati)))))
 
-(defun rogue/init-switch-window ()
-  (use-package switch-window
-    :config
-    (global-set-key (kbd "C-x o") 'switch-window)
-    (setq switch-window-shortcut-style 'qwerty
-          switch-window-qwerty-shortcuts '("a" "s" "d" "f" "j" "k" "l" ";" "w" "e" "i" "o")
-          switch-window-minibuffer-shortcut ?z)))
+(rpkg swiper
+  :bind (("C-s" . swiper)
+         ("C-r" . swiper)))
 
-(defun rogue/init-viz ()
-  (use-package viz
-    :defer t))
+(rpkg switch-window
+  :config
+  (global-set-key (kbd "C-x o") 'switch-window)
+  (setq switch-window-shortcut-style 'qwerty
+        switch-window-qwerty-shortcuts '("a" "s" "d" "f" "j" "k" "l" ";" "w" "e" "i" "o")
+        switch-window-minibuffer-shortcut ?z))
 
-(defun rogue/init-vue-mode ()
-  (use-package vue-mode
-    :mode ("\\.vue\\'" . vue-mode)))
+(rpkg viz)
 
-(defun rogue/init-w ()
-  (use-package w))
+(rpkg vue-mode
+  :mode ("\\.vue\\'" . vue-mode))
 
-(defun rogue/init-writegood-mode ()
-  (use-package writegood-mode
-    :defer t))
+(rpkg w)
+
+(rpkg writegood-mode)
