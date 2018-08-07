0f24abfe518be01e9c8d7144bde05a1be3e41323
Abhinav Tushar <lepisma@fastmail.com>
2018-08-08 03:28:24 +0530

    Remove shitty list

--- packages.el
+++ packages.el
@@ -1,70 +1,18 @@
 ;;; packages.el --- rogue Layer packages File for Spacemacs
 
+(defvar rogue-packages nil)
+
 (defmacro rpkg (name &rest body)
   (declare (indent defun))
-  `(defun ,(intern (format "rogue/init-%s" name)) ()
-     (use-package ,name ,@body)))
+  (let ((id (if (listp name) (car name) name)))
+    `(progn
+       (defun ,(intern (format "rogue/init-%s" id)) ()
+         (use-package ,id ,@body))
+       (push ',name rogue-packages))))
 
-(defconst rogue-packages
-  '(all-the-icons
-    (authinfo :location local)
-    beacon
-    (bmp :location (recipe :fetcher github :repo "lepisma/bmp"))
-    browse-at-remote
-    cricbuzz
-    (calibre :location (recipe :fetcher github :repo "lepisma/calibre.el"))
-    colormaps
-    company-box
-    conda
-    dired-subtree
-    doom-themes
-    enlive
-    gscholar-bibtex
-    hy-mode
-    (kindle :location local)
-    (levenshtein :location (recipe :fetcher github :repo "emacsorphanage/levenshtein"))
-    (mpm :location (recipe :fetcher url :url "https://raw.githubusercontent.com/lepisma/mpm/master/emacs/mpm.el"))
-    multiple-cursors
-    ob-async
-    ob-sagemath
-    openwith
-    (org-bbq :location (recipe :fetcher url :url "https://raw.githubusercontent.com/lepisma/bbq/master/emacs/org-bbq.el"))
-    (org-books :location (recipe :fetcher github :repo "lepisma/org-books"))
-    org-cliplink
-    (org-expand :location (recipe :fetcher github :repo "lepisma/org-expand"))
-    (org-gh :location local)
-    org-journal
-    (org-pretty-table :location (recipe :fetcher github :repo "Fuco1/org-pretty-table"))
-    (org-make :location local)
-    org-super-agenda
-    org-web-tools
-    parinfer
-    (pile :location (recipe :fetcher github :repo "lepisma/pile"))
-    pretty-mode
-    (read-lyrics :location (recipe :fetcher github :repo "lepisma/read-lyrics.el"))
-    realgud
-    (rogue-ligatures :location local)
-    (rogue-mu4e :location local)
-    (rogue-org :location local)
-    (rogue-processes :location local)
-    (rogue-ui :location local)
-    (rogue-utils :location local)
-    sage-shell-mode
-    shell-switcher
-    snakemake-mode
-    solaire-mode
-    (spaceline-all-the-icons :location local)
-    swiper
-    switch-window
-    (viz :location local)
-    vue-mode
-    (w :location (recipe :fetcher github :repo "lepisma/w.el"))
-    writegood-mode))
-
-;; Initialize packages
 (rpkg all-the-icons)
 
-(rpkg authinfo
+(rpkg (authinfo :location local)
   :after (s dash-functional))
 
 (rpkg beacon
@@ -74,13 +22,13 @@
         beacon-blink-when-buffer-changes t
         beacon-blink-when-point-moves-vertically nil))
 
-(rpkg bmp)
+(rpkg (bmp :location (recipe :feature github :repo "lepisma/bmp")))
 
 (rpkg browse-at-remote)
 
 (rpkg cricbuzz)
 
-(rpkg calibre
+(rpkg (calibre :location (recipe :fetcher github :repo "lepisma/calibre.el"))
   :after (s dash-functional)
   :config
   (setq calibre-root (concat user-cloud-dir "Calibre Shared")))
@@ -126,13 +74,15 @@
 (rpkg hy-mode
   :mode "\\.hy\\'")
 
-(rpkg kindle
+(rpkg (kindle :location local)
   :config
   (setq kindle-clipping-save-file user-clippings-file))
 
-(rpkg levenshtein)
+(rpkg (levenshtein :location (recipe :fetcher github :repo "emacsorphanage/levenshtein")))
 
-(rpkg mpm
+(rpkg (mpm :location
+           (recipe :fetcher url
+                   :url "https://raw.githubusercontent.com/lepisma/mpm/master/emacs/mpm.el"))
   :demand t
   :after org-cliplink)
 
@@ -166,20 +116,22 @@
                '(file))))
   (openwith-mode))
 
-(rpkg org-bbq
+(rpkg (org-bbq :location
+               (recipe :fetcher url
+                       :url "https://raw.githubusercontent.com/lepisma/bbq/master/emacs/org-bbq.el"))
   :after org)
 
-(rpkg org-books
+(rpkg (org-books :location (recipe :fetcher github :repo "lepisma/org-books"))
   :config
   (setq org-books-file user-books-file))
 
 (rpkg org-cliplink
   :bind (("C-c y" . org-cliplink)))
 
-(rpkg org-expand
+(rpkg (org-expand :location (recipe :fetcher github :repo "lepisma/org-expand"))
   :bind (("C-c x" . helm-org-expand)))
 
-(rpkg org-gh
+(rpkg (org-gh :location local)
   :after org)
 
 (rpkg org-journal
@@ -187,12 +139,12 @@
   (setq org-journal-dir user-journal-dir)
   (setq org-journal-enable-encryption t))
 
-(rpkg org-pretty-table
+(rpkg (org-pretty-table :location (recipe :fetcher github :repo "Fuco1/org-pretty-table"))
   :demand t
   :init
   (add-hook 'org-mode-hook #'org-pretty-table-mode))
 
-(rpkg org-make
+(rpkg (org-make :location local)
   :after org)
 
 (rpkg org-super-agenda
@@ -218,53 +170,7 @@
                                smart-tab
                                smart-yank)))
 
-(rpkg pretty-mode
-  :config
-  (global-pretty-mode t)
-  (global-prettify-symbols-mode 1)
-
-  (pretty-deactivate-groups
-   '(:equality
-     :ordering
-     :ordering-double
-     :ordering-triple
-     :arrows
-     :arrows-twoheaded
-     :punctuation
-     :logic
-     :sets
-     :sub-and-superscripts
-     :subscripts
-     :arithmetic-double
-     :arithmetic-triple))
-
-  (pretty-activate-groups
-   '(:greek :arithmetic-nary)))
-
-(rpkg read-lyrics
-  :after (s spotify levenshtein))
-
-(rpkg realgud)
-
-(rpkg rogue-ligatures
-  :after rogue-utils
-  :config
-  (rogue-ligatures-setup-general)
-  (rogue-ligatures-setup-ess))
-
-(rpkg rogue-mu4e
-  :after (authinfo mu4e openwith)
-  :config (rogue-mu4e-setup))
-
-(rpkg rogue-org
-  :after org
-  :config
-  (rogue-org-setup-general)
-  (rogue-org-setup-notes)
-  (rogue-org-setup-babel)
-  (rogue-org-setup-tex))
-
-(rpkg pile
+(rpkg (pile :location (recipe :fetcher github :repo "lepisma/pile"))
   :after w
   :config
   (let ((preamble-template "<header>
@@ -324,7 +230,53 @@
                               :preamble "")))
     (pile-setup)))
 
-(rpkg rogue-processes
+(rpkg pretty-mode
+  :config
+  (global-pretty-mode t)
+  (global-prettify-symbols-mode 1)
+
+  (pretty-deactivate-groups
+   '(:equality
+     :ordering
+     :ordering-double
+     :ordering-triple
+     :arrows
+     :arrows-twoheaded
+     :punctuation
+     :logic
+     :sets
+     :sub-and-superscripts
+     :subscripts
+     :arithmetic-double
+     :arithmetic-triple))
+
+  (pretty-activate-groups
+   '(:greek :arithmetic-nary)))
+
+(rpkg (read-lyrics :location (recipe :fetcher github :repo "lepisma/read-lyrics.el"))
+  :after (s spotify levenshtein))
+
+(rpkg realgud)
+
+(rpkg (rogue-ligatures :location local)
+  :after rogue-utils
+  :config
+  (rogue-ligatures-setup-general)
+  (rogue-ligatures-setup-ess))
+
+(rpkg (rogue-mu4e :location local)
+  :after (authinfo mu4e openwith)
+  :config (rogue-mu4e-setup))
+
+(rpkg (rogue-org :location local)
+  :after org
+  :config
+  (rogue-org-setup-general)
+  (rogue-org-setup-notes)
+  (rogue-org-setup-babel)
+  (rogue-org-setup-tex))
+
+(rpkg (rogue-processes :location local)
   :after rogue-utils
   :config
   (rogue-processes-define "offlineimap" "-o")
@@ -332,12 +284,12 @@
   (setq rogue-processes-git-update-dirs nil)
   (rogue-processes-run-git-autoupdate-loop "10 min" 3600))
 
-(rpkg rogue-ui
+(rpkg (rogue-ui :location local)
   :after rogue-utils
   :config
   (rogue-ui-setup))
 
-(rpkg rogue-utils)
+(rpkg (rogue-utils :location local))
 
 (rpkg sage-shell-mode
   :config
@@ -359,10 +311,7 @@
          (minibuffer-setup . solaire-mode-in-minibuffer)
          (ediff-prepare-buffer . solaire-mode)))
 
-(rpkg spaceline
-  :after powerline)
-
-(rpkg spaceline-all-the-icons
+(rpkg (spaceline-all-the-icons :location local)
   :after spaceline
   :config
   (setq-default mode-line-format '("%e" (:eval (spaceline-ml-ati)))))
@@ -378,11 +327,11 @@
         switch-window-qwerty-shortcuts '("a" "s" "d" "f" "j" "k" "l" ";" "w" "e" "i" "o")
         switch-window-minibuffer-shortcut ?z))
 
-(rpkg viz)
+(rpkg (viz :location local))
 
 (rpkg vue-mode
   :mode ("\\.vue\\'" . vue-mode))
 
-(rpkg w)
+(rpkg (w :location (recipe :fetcher github :repo "lepisma/w.el")))
 
 (rpkg writegood-mode)
