4b3f05216022c810c7a1a9bbccf2486da7081aed
Abhinav Tushar <lepisma@fastmail.com>
2018-08-11 08:21:57 +0530

    Remove secrets dir

--- setup.sh
+++ setup.sh
@@ -4,6 +4,3 @@ git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 git clone git@github.com:lepisma/rogue ~/.emacs.d/private/rogue
 ln -s ~/.emacs.d/private/rogue/.spacemacs ~/.spacemacs
 touch ~/.emacs-custom.el
-
-# Secret files stubs
-mkdir -p ~/.emacs.d/private/rogue/secrets
