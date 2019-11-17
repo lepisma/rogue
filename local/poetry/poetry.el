c8be1c17181f3746dc1f81f2961cf1fd2d5eb6e9
Abhinav Tushar <abhinav@lepisma.xyz>
2019-11-17 14:04:49 +0530

    command for allowing linking global packages in poetry venvs

--- local/poetry/poetry.el
+++ local/poetry/poetry.el
@@ -32,7 +32,7 @@
 (require 'f)
 (require 'helm)
 
-(defcustom poetry-venv-dir "~/.cache/pypoetry/virtualenvs/"
+(defcustom poetry-venv-root "~/.cache/pypoetry/virtualenvs/"
   "Path to the virtual environment directory for poetry.")
 
 (defcustom poetry-global-site-packages-dir "~/.pyenv/versions/3.6.8/lib/python3.6/site-packages/"
@@ -42,10 +42,10 @@
 (defun poetry-activate ()
   "Activate a poetry virtual environment."
   (interactive)
-  (let* ((envs (directory-files poetry-venv-dir nil "^[a-z]")))
+  (let* ((envs (directory-files poetry-venv-root nil "^[a-z]")))
     (helm :sources (helm-build-sync-source "virtualenvs"
                      :candidates envs
-                     :action `(("Activate venv" . (lambda (env) (pyvenv-activate (f-join (f-expand ,poetry-venv-dir) env))))))
+                     :action `(("Activate venv" . (lambda (env) (pyvenv-activate (f-join (f-expand ,poetry-venv-root) env))))))
           :buffer "*helm poetry*"
           :prompt "Activate : ")))
 
@@ -54,16 +54,43 @@
   (interactive)
   (pyvenv-deactivate))
 
+(defun poetry-venv-python-version (venv-dir)
+  "Return python version used in VENV-DIR. This relies on
+poetry's naming conventions."
+  (string-match "py\\([0-9]\.[0-9]\\)/?$" venv-dir)
+  (match-string-no-properties 1 venv-dir))
+
+(defun poetry-venv-site-packages-dir (venv-dir)
+  "Return path to site packages for given poetry VENV-DIR."
+  (let ((python-version (poetry-venv-python-version venv-dir)))
+    (concat (file-name-as-directory venv-dir) "lib/python" python-version "/site-packages/")))
+
 (defun poetry-ensure-activation ()
   (unless pyvenv-virtual-env
     (error "No virtualenv activate.")))
+
+(defun poetry-list-packages (site-packages-dir)
+  "List package names, along with properties, available in given
+SITE-PACKAGES-DIR."
+  ;; TODO: Have better return items from here. At present, we just returns
+  ;;       directory entries, putting the burden of identifying packages on the
+  ;;       user.
+  (mapcar (lambda (entry) (cons (f-filename entry) entry)) (directory-files site-packages-dir t "^[a-zA-Z_-]")))
 
 ;;;###autoload
 (defun poetry-link-global ()
   "Pull in global package from site-packages in current
 virtualenv."
   (interactive)
-  (poetry-ensure-activation))
+  (poetry-ensure-activation)
+  (let* ((items (poetry-list-packages poetry-global-site-packages-dir))
+         (picked (helm :sources (helm-build-sync-source "Global items"
+                                  :candidates items)))
+         (link-path (f-join (poetry-venv-site-packages-dir pyvenv-virtual-env) (f-filename picked))))
+    (if (f-exists? link-path)
+        (error "Link path %s already exists" link-path)
+      (make-symbolic-link picked link-path)
+      (message "Linking at %s" link-path))))
 
 ;;;###autoload
 (defun poetry-unlink-global ()
