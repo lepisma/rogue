0b38c72631e94125dc324687dddefc5290c27dd0
Abhinav Tushar <abhinav@lepisma.xyz>
2020-05-16 00:14:14 +0530

    org-roam

--- packages.el
+++ packages.el
@@ -191,6 +191,16 @@
 (r|pkg (org-make :location local)
   :after org)
 
+(r|pkg org-roam
+  :hook (after-init . org-roam-mode)
+  :custom (org-roam-directory (concat user-notes-dir "slum/"))
+  :bind (:map org-roam-mode-map
+              (("C-c n l" . org-roam)
+               ("C-c n f" . org-roam-find-file)
+               ("C-c n g" . org-roam-show-graph))
+              :map org-mode-map
+              (("C-c n i" . org-roam-insert))))
+
 (r|pkg org-super-agenda
   :after org
   :config
