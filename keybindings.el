f070a5da4f61d69d710090f668a993a445b8399d
Abhinav Tushar <abhinav@lepisma.xyz>
2020-06-11 01:48:51 +0530

    my life is complete

--- keybindings.el
+++ keybindings.el
@@ -51,6 +51,7 @@
 (global-set-key (kbd "M-u") 'sp-backward-unwrap-sexp)
 (global-set-key (kbd "M-n") 'sp-end-of-sexp)
 (global-set-key (kbd "M-p") 'sp-beginning-of-sexp)
+(global-set-key (kbd "M-k") 'sp-kill-sexp)
 
 ;; Persp grouping
 (global-set-key (kbd "C-x C-b") #'(lambda (arg) (interactive "P") (with-persp-buffer-list () (ibuffer arg))))
