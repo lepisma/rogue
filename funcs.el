b9663ac579d1457e5227b6048c591492bc721496
Abhinav Tushar <abhinav@lepisma.xyz>
2020-11-22 14:35:15 +0530

    clean up funs

--- funcs.el
+++ funcs.el
@@ -1,7 +1,5 @@
 ;;; funcs.el --- rogue Layer utility functions -*- lexical-binding: t -*-
 
-(defvar ev-root "~/.config/env/env")
-
 (defun to-fish-find-file (candidate)
   "Run find file for given bookmark"
   (helm-find-files-1 (file-name-as-directory (f-canonical (f-join "~/.tofish" candidate)))))
@@ -17,31 +15,6 @@
                    :action '(("Jump to bookmark" . to-fish-find-file)))
         :buffer "*helm tofish jump*"
         :prompt "Jump to: "))
-
-(defun ev-set-group (group-name)
-  (let ((dir (f-join ev-root group-name)))
-    (dolist (file (dir-items dir))
-      (setenv file (s-trim (f-read-text (f-join dir file)))))))
-
-(defun ev-reset-group (group-name)
-  (let ((dir (f-join ev-root group-name)))
-    (dolist (file (dir-items dir))
-      (setenv file nil))))
-
-(defun ev-workon ()
-  "Set ev environment variables"
-  (interactive)
-  (helm :sources (helm-build-sync-source "groups"
-                   :candidates (dir-items ev-root)
-                   :action '(("Set vars" . ev-set-group)))
-        :buffer "*helm ev workon*"
-        :prompt "ev workon: "))
-
-(defun ev-reset ()
-  "Clear whatever ev environment vars are set"
-  (interactive)
-  (dolist (group-name (dir-items ev-root))
-    (ev-reset-group group-name)))
 
 (defun delete-word (arg)
   "Delete characters forward until encountering the end of a word.
@@ -87,8 +60,7 @@ With argument, do this that many times."
   (disable-theme r-dark-theme)
   (spacemacs/load-theme r-light-theme)
   (setq org-bullets-bullet-list '(" "))
-  (r-org/reset-buffers)
-  (beacon-mode -1))
+  (r-org/reset-buffers))
 
 (defun r/dark ()
   "Switch to dark theme"
@@ -96,24 +68,7 @@ With argument, do this that many times."
   (disable-theme r-light-theme)
   (spacemacs/load-theme r-dark-theme)
   (setq org-bullets-bullet-list '("› "))
-  (r-org/reset-buffers)
-  (beacon-mode +1))
-
-(defun quack-quack (text)
-  "Speak the given text"
-  (start-process "quack" nil "quack" text))
-
-(defun quack-unread-mail ()
-  "Read unread emails"
-  (interactive)
-  (let ((subjects (mapcar
-                   (lambda (mail) (concat (plist-get mail :subject) ", from " (caar (plist-get mail :from))))
-                   (r-mu4e/get-unread-mails))))
-    (quack-quack (format "You have %s. %s"
-                         (cond ((= (length subjects) 0) "no unread emails")
-                               ((= (length subjects) 1) "1 unread email")
-                               (t (format "%s unread e mails" (length subjects))))
-                         (s-join ". " subjects)))))
+  (r-org/reset-buffers))
 
 (defun magit-bookmarks ()
   (interactive)
@@ -132,12 +87,6 @@ With argument, do this that many times."
   (with-eval-after-load 'magit
       (transient-append-suffix 'magit-commit "c"
         '("g" "Commit with generic message" magit-commit-generic-update))))
-
-(defun clear-entity (text entity)
-  "Remove the ranged ENTITY from TEXT."
-  (let* ((match-range (alist-get 'range entity))
-         (match (substring-no-properties text (car match-range) (cdr match-range))))
-    (s-trim (s-collapse-whitespace (s-replace match "" text)))))
 
 (defun screenshot-svg ()
   "Save a screenshot of the current frame as an SVG image.
