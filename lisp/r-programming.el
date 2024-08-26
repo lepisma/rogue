d941c1da0860b35b78bc0133ff6ca689db3875a1
Abhinav Tushar <abhinav@lepisma.xyz>
2024-08-26 11:06:40 +0530

    Show trailing whitespace in prog mode

--- lisp/r-programming.el
+++ lisp/r-programming.el
@@ -32,6 +32,7 @@
 (add-hook 'prog-mode-hook #'display-line-numbers-mode)
 (add-hook 'prog-mode-hook #'hl-line-mode)
 (add-hook 'prog-mode-hook (lambda () (setq line-spacing 0.1)))
+(add-hook 'prog-mode-hook (lambda () (setq show-trailing-whitespace t)))
 
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
