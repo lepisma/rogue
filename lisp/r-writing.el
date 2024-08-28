9807363057e9f769e1d77c1bf637e72daeee7fe2
Abhinav Tushar <abhinav@lepisma.xyz>
2024-08-28 16:07:44 +0530

    Pile setup

--- lisp/r-writing.el
+++ lisp/r-writing.el
@@ -119,8 +119,103 @@
   (require 'org-roam-protocol)
   (org-roam-db-autosync-mode))
 
+;; Primarily for serving `pile' pages
 (use-package w
   :vc (:fetcher github :repo lepisma/w.el))
+
+;; This is for some template rendering for `pile'
+(use-package mustache)
+
+(use-package pile
+  :vc (:fetcher github :repo lepisma/pile)
+  :after (mustache w)
+  :config
+  (let* ((template-dir (concat  "misc/"))
+         (preamble-template "<header>
+  <div class='site-title'>
+    <a href='/'>
+      <img src='/assets/images/avatar32.png'>
+    </a>
+  </div>
+  <div class='site-nav'>
+    <a {{#blog-p}}class='active'{{/blog-p}} href='/'> blog</a>
+    <a {{#journal-p}}class='active'{{/journal-p}} href='/journal'> journal</a>
+    <a {{#log-p}}class='active'{{/log-p}} href='/log'> log</a>
+    <a {{#wiki-p}}class='active'{{/wiki-p}} href='/wiki'> wiki</a>
+    <a href='/about'> about</a>
+  </div>
+  <div class='clearfix'></div>
+</header>
+
+<div class='page-header'>
+  <div class='page-meta'>{{page-meta}}</div>
+  <h1>%t</h1>
+</div>")
+         (postamble-template "<footer id='footer'></footer>")
+         (root-url "https://lepisma.xyz/")
+         (input-dir (concat user-cloud-dir "lepisma.github.io/"))
+         (output-dir (concat user-cloud-dir "projects/lepisma.github.io-deploy/")))
+    (setq pile-serve-dir output-dir
+          pile-projects
+          (list (pile-project-wiki :name "wiki"
+                                   :root-url root-url
+                                   :base-url "wiki"
+                                   :input-dir (concat input-dir "wiki")
+                                   :output-dir (concat output-dir "wiki")
+                                   :postamble postamble-template
+                                   :preamble (mustache-render preamble-template '(("wiki-p" . t) ("page-meta" . "Last modified: %d %C")))
+                                   :setupfile (concat input-dir "assets/export.setup"))
+                (pile-project-blog :name "blog"
+                                   :root-url root-url
+                                   :base-url ""
+                                   :input-dir (concat input-dir "blog")
+                                   :output-dir output-dir
+                                   :postamble postamble-template
+                                   :preamble (mustache-render preamble-template '(("blog-p" . t) ("page-meta" . "%d")))
+                                   :setupfile (concat input-dir "assets/export.setup"))
+                (pile-project-blog :name "journal"
+                                   :root-url root-url
+                                   :base-url "journal"
+                                   :input-dir (concat input-dir "journal")
+                                   :output-dir (concat output-dir "journal")
+                                   :postamble postamble-template
+                                   :preamble (mustache-render preamble-template '(("journal-p" . t) ("page-meta" . "%d")))
+                                   :setupfile (concat input-dir "assets/export.setup"))
+                (pile-project-blog :name "log"
+                                   :root-url root-url
+                                   :base-url "log"
+                                   :input-dir (concat input-dir "log")
+                                   :output-dir (concat output-dir "log")
+                                   :postamble postamble-template
+                                   :preamble (mustache-render preamble-template '(("log-p" . t) ("page-meta" . "%d"))))
+                (pile-project-static :name "assets"
+                                     :root-url root-url
+                                     :input-dir (concat input-dir "assets")
+                                     :output-dir (concat output-dir "assets"))
+                (pile-project-plain :name "misc"
+                                    :root-url root-url
+                                    :base-url ""
+                                    :input-dir (concat input-dir "misc")
+                                    :output-dir output-dir)))
+    (pile-setup)
+    ;; Notice that sequence of hooks is important here
+    (dolist (fn (list #'pile-hooks-pre-add-setupfile
+                      #'pile-hooks-pre-add-bc
+                      #'pile-hooks-pre-add-cids
+                      #'pile-hooks-pre-add-date
+                      #'pile-hooks-pre-add-dropcap
+                      #'pile-hooks-pre-add-tags
+                      #'pile-hooks-pre-add-crosslinks
+                      #'pile-hooks-pre-add-draft-watermark))
+      (add-hook 'pile-pre-publish-hook fn t))
+    (dolist (fn (list #'pile-hooks-post-clear-cids
+                      #'pile-hooks-post-generate-atom
+                      #'pile-hooks-post-generate-archive
+                      #'pile-hooks-post-stringify-title
+                      #'pile-hooks-post-sync-static-files
+                      #'pile-hooks-post-generate-index))
+      (add-hook 'pile-post-publish-hook fn t))))
+
 (provide 'r-writing)
 
 ;;; r-writing.el ends here
