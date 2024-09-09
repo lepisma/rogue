9154542c9bd5278571a6bd2b24547846da964afa
Abhinav Tushar <abhinav@lepisma.xyz>
2024-09-09 17:59:36 +0530

    Remove unused keys

--- lisp/r-writing.el
+++ lisp/r-writing.el
@@ -336,8 +336,7 @@
   (esi-dictate-dg-api-key (auth-info-password (car (auth-source-search :host "deepgram"))))
   (esi-dictate-llm-provider (make-llm-openai :key (auth-info-password (car (auth-source-search :host "api.openai.com"))) :chat-model "gpt-4o-mini"))
   :bind (:map esi-dictate-mode-map
-              ("C-g" . esi-dictate-stop)
-              ("C-<return>" . esi-dictate-fix-last))
+              ("C-g" . esi-dictate-stop))
   :config
   (setq llm-warn-on-nonfree nil)
   :hook (esi-dictate-speech-final . esi-dictate-fix-last))
