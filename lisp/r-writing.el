566ee1a2e3e332c15ceddcc373698929b0e2956c
Abhinav Tushar <abhinav@lepisma.xyz>
2024-09-10 15:56:55 +0530

    Use function reader syntax for text-mode hooks

--- lisp/r-writing.el
+++ lisp/r-writing.el
@@ -26,8 +26,8 @@
 
 ;;; Code:
 
-(add-hook 'text-mode-hook 'auto-fill-mode)
-(add-hook 'text-mode-hook 'flyspell-mode)
+(add-hook 'text-mode-hook #'auto-fill-mode)
+(add-hook 'text-mode-hook #'flyspell-mode)
 (add-hook 'org-mode-hook (lambda () (setq line-spacing 0.2)))
 
 (use-package markdown-mode)
