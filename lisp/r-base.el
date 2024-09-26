94eae7bdb0ed09789e1f547e75460c3076713ef8
Abhinav Tushar <abhinav@lepisma.xyz>
2024-09-26 09:09:05 +0530

    Switch to bufler for buffer management

--- lisp/r-base.el
+++ lisp/r-base.el
@@ -289,6 +289,68 @@ default face height is set to 0.1 to hide regular prompt/contents"
   :config
   (global-set-key (kbd "C-x o") 'switch-window))
 
+(use-package bufler
+  :bind ("C-x C-b" . bufler)
+  :custom
+  (bufler-groups
+   (bufler-defgroups
+     (group
+      ;; Subgroup collecting all `help-mode' and `info-mode' buffers.
+      (group-or "*Help/Info*"
+                (mode-match "*Help*" (rx bos "help-"))
+                (mode-match "*Info*" (rx bos "info-"))))
+     (group
+      ;; Subgroup collecting all special buffers (i.e. ones that are not
+      ;; file-backed), except `magit-status-mode' buffers (which are allowed to fall
+      ;; through to other groups, so they end up grouped with their project buffers).
+      (group-and "*Special*"
+                 (lambda (buffer)
+                   (unless (or (funcall (mode-match "Magit" (rx bos "magit-status"))
+                                        buffer)
+                               (funcall (mode-match "Dired" (rx bos "dired"))
+                                        buffer)
+                               (funcall (auto-file) buffer))
+                     "*Special*")))
+      (group
+       ;; Subgroup collecting these "special special" buffers
+       ;; separately for convenience.
+       (name-match "**Special**"
+                   (rx bos "*" (or "Messages" "Warnings" "scratch" "Backtrace") "*")))
+      (group
+       ;; Subgroup collecting all other Magit buffers, grouped by directory.
+       (mode-match "*Magit* (non-status)" (rx bos (or "magit" "forge") "-"))
+       (auto-directory))
+      ;; Subgroup for Helm buffers.
+      (mode-match "*Helm*" (rx bos "helm-"))
+      ;; Remaining special buffers are grouped automatically by mode.
+      (auto-mode))
+     (group
+      ;; Subgroup collecting buffers in `org-directory' (or "~/org" if
+      ;; `org-directory' is not yet defined).
+      (dir (if (bound-and-true-p org-directory)
+               org-directory
+             "~/org"))
+      (group
+       ;; Subgroup collecting indirect Org buffers, grouping them by file.
+       ;; This is very useful when used with `org-tree-to-indirect-buffer'.
+       (auto-indirect)
+       (auto-file))
+      ;; Group remaining buffers by whether they're file backed, then by mode.
+      (group-not "*special*" (auto-file))
+      (auto-mode))
+     (group
+      ;; Subgroup collecting buffers in a projectile project.
+      (auto-projectile))
+     (group
+      ;; Subgroup collecting buffers in a version-control project,
+      ;; grouping them by directory.
+      (auto-project))
+     (dir user-emacs-directory)
+     (dir user-notes-dir)
+     (dir (concat user-cloud-dir "lepisma.github.io/"))
+     (auto-mode)))
+  (bufler-reverse t))
+
 ;; Personal navigation patterns
 (defun delete-word (arg)
   "Delete characters forward until encountering the end of a word.
